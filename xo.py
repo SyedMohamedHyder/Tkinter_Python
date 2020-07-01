af170da1e55ca2f21d04aa6fc856ffe4cdadbaab
SyedMohamedHyder <syedhyder1362k@gmail.com>
2020-07-01 18:18:20 +0530

    Completed xo.py file

--- xo.py
+++ xo.py
@@ -2,13 +2,19 @@
 
 #-----------Global Variables-----------
 
+
+import sys
+
+
 board = ["-","-","-",
          "-","-","-",
          "-","-","-"]
 
 
-def display_board():
 
+
+def display_board():
+    print("\n \n")
     print(board[0]+"|"+board[1]+"|"+board[2])
     print(board[3]+"|"+board[4]+"|"+board[5])
     print(board[6]+"|"+board[7]+"|"+board[8])
@@ -17,34 +23,76 @@ def display_board():
 def main():
 
     display_board()
+    print("\n \n press \"X\" to exit the game \n \n")
+    while game_not_over():
 
-    while True:
 
+       if not winner():
+          x_chance()
+          display_board()
+       else:
+          return "\n \n **************** Player \"O\" is Winner ****************\n \n"
 
-        x_chance()
-        display_board()
-        o_chance()
-        display_board()
+       if not winner() and game_not_over():
+          o_chance()
+          display_board()
+       elif winner():
+          return "\n \n *************** Player \"X\" is Winner **************** \n \n"
+
+    return "\n \n *************** (: DRAW THE MATCH :) ****************\n \n"
 
 
 def x_chance():
 
+    global board
     position=input("Where do you want to insert \"X\" : ")
-    position=int(position)-1
-    if position<len(board) and board[position]=="-" :
-       board[position]="X"
-    else :
-       print ("Enter a valid position")
+    if position.lower()=="x":
+       sys.exit("\n \n Game Stopped \n \n ")
+      
+    try:
+
+       position=int(position)-1
+       if position<len(board) and board[position]=="-" :
+          board[position]="X"
+       else :
+          print ("\n Enter a valid position \n")
+          x_chance()
+
+    except:
+       print ("\n Enter a valid position \n")
        x_chance()
 
 def o_chance():
 
+    global board
     position=input("Where do you want to insert \"O\" : ")
-    position=int(position)-1
-    if position<len(board) and board[position]=="-" :
-       board[position]="O"
-    else :
-       print ("Enter a valid position")
+
+    if position.lower()=="x":
+       sys.exit("\n \n Game Stopped \n \n ")
+    try:
+       position=int(position)-1
+       if position<len(board) and board[position]=="-" :
+          board[position]="O"
+       else :
+          print ("\n Enter a valid position \n")
+          o_chance()
+    except:
+       print ("\n Enter a valid position \n")
        o_chance()
 
-main()
+
+
+def game_not_over():
+
+
+   return board.count("-")>=1 and board.count("-")<=9
+
+
+def winner():
+
+   return (board[0]!="-" and board[0]==board[1] and board[1]==board[2]) or (board[3]!="-" and board[3]==board[4] and board[4]==board[5]) or (board[6]!="-" and board[6]==board[7] and board[7]==board[8]) or (board[0]!="-" and board[0]==board[3] and board[3]==board[6]) or (board[1]!="-" and board[1]==board[4] and board[4]==board[7]) or (board[2]!="-" and board[2]==board[5] and board[5]==board[8]) or (board[0]!="-" and board[0]==board[4] and board[4]==board[8]) or (board[2]!="-" and board[2]==board[4] and board[4]==board[6])
+
+
+
+
+print (main())
