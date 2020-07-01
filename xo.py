2109add23da88e3e4a59fcc8ba68008771e8da0b
SyedMohamedHyder <syedhyder1362k@gmail.com>
2020-07-01 13:34:11 +0530

    Added an empty board and a display_board() function to print the board to the xo.py file

--- xo.py
+++ xo.py
@@ -1,3 +1,17 @@
-def git_opeation():
- print("I am adding example.py file to the remote repository.")
-git_opeation()
+#!/c/Users/SYED/AppData/Local/Programs/Python/Python38/python
+
+#-----------Global Variables-----------
+
+board = ["-","-","-",
+         "-","-","-",
+         "-","-","-"]
+
+
+def display_board():
+
+    print(board[0]+"|"+board[1]+"|"+board[2])
+    print(board[3]+"|"+board[4]+"|"+board[5])
+    print(board[3]+"|"+board[4]+"|"+board[5])
+
+
+display_board()
