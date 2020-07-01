c1968ac02845607b41831a56aec00a890297a2f8
SyedMohamedHyder <syedhyder1362k@gmail.com>
2020-07-01 19:09:07 +0530

    Fixed some bugs related to closing the file using "X"
    When we enter an invalid number , we call the function again
    Now when we try to exit the function by typing "x" ,
    sys.exit() exits only from the current function.
    It doesn't stop the program instead. So instead of using "x" to exit ,
    we just inform the user to use "Ctrl+x" to exit the program.

--- xo.py
+++ xo.py
@@ -4,51 +4,26 @@
 
 
 import sys
-
+import os
 
 board = ["-","-","-",
          "-","-","-",
          "-","-","-"]
 
 
-
-
 def display_board():
-    print("\n \n")
-    print(board[0]+"|"+board[1]+"|"+board[2])
-    print(board[3]+"|"+board[4]+"|"+board[5])
-    print(board[6]+"|"+board[7]+"|"+board[8])
-
-
-def main():
-
-    display_board()
-    print("\n \n press \"X\" to exit the game \n \n")
-    while game_not_over():
-
-
-       if not winner():
-          x_chance()
-          display_board()
-       else:
-          return "\n \n **************** Player \"O\" is Winner ****************\n \n"
-
-       if not winner() and game_not_over():
-          o_chance()
-          display_board()
-       elif winner():
-          return "\n \n *************** Player \"X\" is Winner **************** \n \n"
-
-    return "\n \n *************** (: DRAW THE MATCH :) ****************\n \n"
-
+    print ("\n \n")
+    print (board[0]+"|"+board[1]+"|"+board[2])
+    print (board[3]+"|"+board[4]+"|"+board[5])
+    print (board[6]+"|"+board[7]+"|"+board[8])
+    print ("\n \n")
 
 def x_chance():
 
     global board
+
     position=input("Where do you want to insert \"X\" : ")
-    if position.lower()=="x":
-       sys.exit("\n \n Game Stopped \n \n ")
-      
+
     try:
 
        position=int(position)-1
@@ -59,16 +34,17 @@ def x_chance():
           x_chance()
 
     except:
+
        print ("\n Enter a valid position \n")
        x_chance()
 
 def o_chance():
 
     global board
+
     position=input("Where do you want to insert \"O\" : ")
 
-    if position.lower()=="x":
-       sys.exit("\n \n Game Stopped \n \n ")
+
     try:
        position=int(position)-1
        if position<len(board) and board[position]=="-" :
@@ -76,6 +52,7 @@ def o_chance():
        else :
           print ("\n Enter a valid position \n")
           o_chance()
+
     except:
        print ("\n Enter a valid position \n")
        o_chance()
@@ -92,6 +69,29 @@ def winner():
 
    return (board[0]!="-" and board[0]==board[1] and board[1]==board[2]) or (board[3]!="-" and board[3]==board[4] and board[4]==board[5]) or (board[6]!="-" and board[6]==board[7] and board[7]==board[8]) or (board[0]!="-" and board[0]==board[3] and board[3]==board[6]) or (board[1]!="-" and board[1]==board[4] and board[4]==board[7]) or (board[2]!="-" and board[2]==board[5] and board[5]==board[8]) or (board[0]!="-" and board[0]==board[4] and board[4]==board[8]) or (board[2]!="-" and board[2]==board[4] and board[4]==board[6])
 
+
+
+
+def main():
+
+    display_board()
+    print("\n \n press \"Ctrl+\\\" to exit the game \n \n")
+    while game_not_over():
+
+
+       if not winner():
+          x_chance()
+          display_board()
+       else:
+          return "\n \n **************** Player \"O\" is Winner ****************\n \n"
+
+       if not winner() and game_not_over():
+          o_chance()
+          display_board()
+       elif winner():
+          return "\n \n *************** Player \"X\" is Winner **************** \n \n"
+
+    return "\n \n *************** (: DRAW THE MATCH :) ****************\n \n"
 
 
 
