21b21ddf58633e1d9ff4feda2db227bc8806664a
SyedMohamedHyder <syedhyder1362k@gmail.com>
2020-07-08 16:55:45 +0530

    Added save function to my_text_editor.py. The program has been completed!
    The program can save files in .docx or .txt or .csv extension only.
    All files option is not made available during saving because it may
    corrupt any file such as VLC or even PDF as file.write() is writing
    its contents to those files also!
    
    So All Files option is not made available for the users!

--- my_text_editor.py
+++ my_text_editor.py
@@ -42,6 +42,33 @@ def openFile():
 
           messagebox.showerror("ERROR","Not a valid file!")
 
+
+
+def saveFile():
+
+    file_path=asksaveasfilename(defaultextension=".txt",filetypes=[("Text files","*.txt"),("All Word Documents","*.docx"),("CSV(Comma delimited)","*.csv")])
+
+    if not file_path :
+
+       return
+
+    text=txt_editor.get("1.0",END)
+
+    if file_path.endswith(".docx"):
+
+       doc=docx.Document()
+       doc.add_paragraph(text)
+       doc.save(file_path)
+
+    else:
+
+       with open (file_path,"w") as file:
+
+          text=txt_editor.get("1.0",END)
+          file.write(text)
+          file.close()
+    window.title("Simple Text Editor - {}".format(file_path))
+
 window=Tk()
 window.title("Simple Text Editor")
 
@@ -52,13 +79,13 @@ buttonFrame=Frame(master=window)
 txt_editor=Text(master=window)
 
 openButton=Button(master=buttonFrame,text="Open",command=openFile)
-saveButton=Button(master=buttonFrame,text="Save as...")
+saveButton=Button(master=buttonFrame,text="Save as...",command=saveFile)
 
 openButton.grid(row=0,column=0,sticky="ew",padx=10,pady=10)
 saveButton.grid(row=1,column=0,sticky="ew",padx=10)
 
 buttonFrame.grid(row=0,column=0,sticky="ns")
 txt_editor.grid(row=0,column=1,sticky="nswe")
-
+txt_editor.focus_set()
 
 window.mainloop()
