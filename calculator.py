06965ba15d76afbf2aa18f213d0474dbde71b74c
SyedMohamedHyder <syedhyder1362k@gmail.com>
2020-07-10 15:45:31 +0530

    Completed the calculator GUI
    Fixed the bugs in calculator.py

--- calculator.py
+++ calculator.py
@@ -2,35 +2,232 @@
 
 from tkinter import *
 
+operation=None
+check=True
+def number(num):
+
+    global check
+    if check:
+       screen_label["text"]=""
+       check=False
+    screen_label["text"]=screen_label["text"]+str(num)
+
+def add():
+    global operation
+    if not screen_label["text"]:
+       operaion="add"
+       return
+    global first_num
+    if operation=="add":
+       sec_num=float(screen_label["text"])
+       first_num=first_num+sec_num
+       screen_label["text"]=""
+    elif operation=="sub":
+       sec_num=float(screen_label["text"])
+       first_num=first_num-sec_num
+       screen_label["text"]=""
+    elif operation=="mul":
+       sec_num=float(screen_label["text"])
+       first_num=first_num*sec_num
+       screen_label["text"]=""
+    elif operation=="div":
+       if float(screen_label["text"])==0:
+          clear()
+          return
+       sec_num=float(screen_label["text"])
+       first_num=first_num/sec_num
+       screen_label["text"]=""
+    else:
+       first_num=float(screen_label["text"])
+       screen_label["text"]=""
+    operation="add"
+
+def subtract():
+
+    global operation
+    if not screen_label["text"]:
+       operation="sub"
+       return
+    global first_num
+    if operation=="add":
+       sec_num=float(screen_label["text"])
+       first_num=first_num+sec_num
+       screen_label["text"]=""
+    elif operation=="sub":
+       sec_num=float(screen_label["text"])
+       first_num=first_num-sec_num
+       screen_label["text"]=""
+    elif operation=="mul":
+       sec_num=float(screen_label["text"])
+       first_num=first_num*sec_num
+       screen_label["text"]=""
+    elif operation=="div":
+       if float(screen_label["text"])==0:
+          clear()
+          return
+       sec_num=float(screen_label["text"])
+       first_num=first_num/sec_num
+       screen_label["text"]=""
+    else:
+       first_num=float(screen_label["text"])
+       screen_label["text"]=""
+    operation="sub"
+
+def multiply():
+
+    global operation
+    if not screen_label["text"]:
+       operation="mul"
+       return
+    global first_num
+    if operation=="add":
+       sec_num=float(screen_label["text"])
+       first_num=first_num+sec_num
+       screen_label["text"]=""
+    elif operation=="sub":
+       sec_num=float(screen_label["text"])
+       first_num=first_num-sec_num
+       screen_label["text"]=""
+    elif operation=="mul":
+       sec_num=float(screen_label["text"])
+       first_num=first_num*sec_num
+       screen_label["text"]=""
+    elif operation=="div":
+       if float(screen_label["text"])==0:
+          clear()
+          return
+       sec_num=float(screen_label["text"])
+       first_num=first_num/sec_num
+       screen_label["text"]=""
+    else:
+       first_num=float(screen_label["text"])
+       screen_label["text"]=""
+    operation="mul"
+
+def divide():
+
+    global operation
+    if not screen_label["text"]:
+       operation="div"
+       return
+    global first_num
+    if operation=="add":
+       sec_num=float(screen_label["text"])
+       first_num=first_num+sec_num
+       screen_label["text"]=""
+    elif operation=="sub":
+       sec_num=float(screen_label["text"])
+       first_num=first_num-sec_num
+       screen_label["text"]=""
+    elif operation=="mul":
+       sec_num=float(screen_label["text"])
+       first_num=first_num*sec_num
+       screen_label["text"]=""
+    elif operation=="div":
+       if float(screen_label["text"])==0:
+          clear()
+          return
+       sec_num=float(screen_label["text"])
+       first_num=first_num/sec_num
+       screen_label["text"]=""
+    else:
+       first_num=float(screen_label["text"])
+       screen_label["text"]=""
+    operation="div"
+
+def clear():
+
+    global operation
+    global first_num
+    screen_label["text"]=""
+    operation=None
+    first_num=None
+
+def equal():
+
+    global first_num
+    global operation
+    global check
+    if not screen_label["text"]:
+       screen_label["text"]=str(first_num)
+       first_num=None
+       operation=None
+       check=True
+       return
+    if operation=="add":
+       sec_num=float(screen_label["text"])
+       first_num=first_num+sec_num
+       screen_label["text"]=""
+    elif operation=="sub":
+       sec_num=float(screen_label["text"])
+       first_num=first_num-sec_num
+       screen_label["text"]=""
+    elif operation=="mul":
+       sec_num=float(screen_label["text"])
+       first_num=first_num*sec_num
+       screen_label["text"]=""
+    elif operation=="div":
+       if float(screen_label["text"])==0:
+          clear()
+          return
+       sec_num=float(screen_label["text"])
+       first_num=first_num/sec_num
+       screen_label["text"]=""
+    else:
+       return
+
+    screen_label["text"]=str(first_num)
+    operation=None
+    check=True
+
+def sqrt():
+    global first_num
+    global operation
+    if not screen_label["text"]:
+       return
+    screen_label["text"]=str(float(screen_label["text"])**0.5)
+    first_num=screen_label["text"]
+    operation=None
+
+def reciprocal():
+
+    global first_num
+    global operation
+    if not screen_label["text"] or float(screen_label["text"])==0:
+       return
+    screen_label["text"]=str(1/(float(screen_label["text"])))
+    first_num=screen_label["text"]
+    operation=None
+
+
 window=Tk()
 window.title("Calculator")
 window.resizable(False,False)
-
 mainFrame=Frame(master=window)
 
 screen_label=Label(master=mainFrame,height=3,bg="white",font="bold",
-                   justify=LEFT,borderwidth=3,relief=RIDGE)
+                   justify=RIGHT,anchor="e",borderwidth=3,relief=RIDGE)
 
-button0=Button(master=mainFrame,text="0",bg="white",width=4,height=3)
-button1=Button(master=mainFrame,text="1",bg="white",width=4,height=3)
-button2=Button(master=mainFrame,text="2",bg="white",width=4,height=3)
-button3=Button(master=mainFrame,text="3",width=4,bg="white",height=3)
-button4=Button(master=mainFrame,text="4",width=4,height=3,bg="white")
-button5=Button(master=mainFrame,text="5",width=4,height=3,bg="white")
-button6=Button(master=mainFrame,text="6",width=4,height=3,bg="white")
-button7=Button(master=mainFrame,text="7",width=4,height=3,bg="white")
-button8=Button(master=mainFrame,text="8",width=4,height=3,bg="white")
-button9=Button(master=mainFrame,text="9",width=4,height=3,bg="white")
+button0=Button(master=mainFrame,text="0",bg="white",width=4,height=3,command=lambda:number(0))
+button1=Button(master=mainFrame,text="1",bg="white",width=4,height=3,command=lambda:number(1))
+button2=Button(master=mainFrame,text="2",bg="white",width=4,height=3,command=lambda:number(2))
+button3=Button(master=mainFrame,text="3",width=4,bg="white",height=3,command=lambda:number(3))
+button4=Button(master=mainFrame,text="4",width=4,height=3,bg="white",command=lambda:number(4))
+button5=Button(master=mainFrame,text="5",width=4,height=3,bg="white",command=lambda:number(5))
+button6=Button(master=mainFrame,text="6",width=4,height=3,bg="white",command=lambda:number(6))
+button7=Button(master=mainFrame,text="7",width=4,height=3,bg="white",command=lambda:number(7))
+button8=Button(master=mainFrame,text="8",width=4,height=3,bg="white",command=lambda:number(8))
+button9=Button(master=mainFrame,text="9",width=4,height=3,bg="white",command=lambda:number(9))
 
-buttonADD=Button(master=mainFrame,text="+",width=4,height=3,bg="white")
-buttonSUB=Button(master=mainFrame,text="-",width=4,height=3,bg="white")
-buttonMUL=Button(master=mainFrame,text="X",width=4,height=3,bg="white")
-buttonDIV=Button(master=mainFrame,text="/",width=4,height=3,bg="white")
+buttonADD=Button(master=mainFrame,text="+",width=4,height=3,bg="white",command=add)
+buttonSUB=Button(master=mainFrame,text="-",width=4,height=3,bg="white",command=subtract)
+buttonMUL=Button(master=mainFrame,text="X",width=4,height=3,bg="white",command=multiply)
+buttonDIV=Button(master=mainFrame,text="/",width=4,height=3,bg="white",command=divide)
 
-buttonCLEAR=Button(master=mainFrame,text="C",width=4,height=3,bg="white")
-buttonEQUAL=Button(master=mainFrame,text="=",width=4,height=3,bg="white")
-buttonX=Button(master=mainFrame,text="1/x",width=4,height=3,bg="white")
-buttonSQRT=Button(master=mainFrame,text="√",width=4,height=3,bg="white")
+buttonCLEAR=Button(master=mainFrame,text="C",width=4,height=3,bg="white",command=clear)
+buttonEQUAL=Button(master=mainFrame,text="=",width=4,height=3,bg="white",command=equal)
+buttonRECIPROCAL=Button(master=mainFrame,text="1/x",width=4,height=3,bg="white",command=reciprocal)
+buttonSQRT=Button(master=mainFrame,text="√",width=4,height=3,bg="white",command=sqrt)
 
 
 button7.grid(row=1,column=0,padx=3,pady=3,sticky="nsew")
@@ -47,7 +244,7 @@ button0.grid(row=4,column=0,columnspan=2,padx=3,pady=3,sticky="nsew")
 buttonCLEAR.grid(row=4,column=2,padx=3,pady=3,sticky="nsew")
 buttonEQUAL.grid(row=3,column=4,rowspan=2,padx=3,pady=3,sticky="nsew")
 buttonSQRT.grid(row=1,column=4,padx=3,pady=3,sticky="nsew")
-buttonX.grid(row=2,column=4,padx=3,pady=3,sticky="nsew")
+buttonRECIPROCAL.grid(row=2,column=4,padx=3,pady=3,sticky="nsew")
 
 buttonADD.grid(row=4,column=3,padx=3,pady=3,sticky="nsew")
 buttonSUB.grid(row=3,column=3,padx=3,pady=3,sticky="nsew")
